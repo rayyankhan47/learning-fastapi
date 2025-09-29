d9f4a542d85d62b1084500c667d369b2d6fcd62a
AhmedSalafi <ryankhann47@gmail.com>
2025-09-28 21:14:31 -0400

    updating endpoints to query db through dependency injection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from models import Product
 from database import SessionLocal, engine
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -18,27 +19,59 @@ products = [
     Product(id=4, name="book", description="interesting book", price=15.00, quantity=20),
 ]
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+     db.close()
 
+def init_db():
+    db = SessionLocal()
+
+    count = db.query(database_models.Product).count
+
+    if count == 0:
+        for product in products:
+            db.add(database_models.Product(**product.model_dump())) # unpacking, ** gives key value pairs from dict
+    
+        db.commit() # we disabled this by default earlier
+
+init_db()
+
+# endpoints
 
 @app.get("/products")
-def get_all_products():
-    # db connection
-    db = SessionLocal()
-    # query
-    db.query
-    return products
+def get_all_products(db: Session = Depends(get_db)):
+
+    # OLD: return products
+
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 @app.get("/product/{id}")
-def get_product_by_id(id: int):
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    '''
+    OLD:
     for product in products:
         if product.id == id:
             return product
     return "product not found"
+    '''
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
+    return "Product not found bro!"
 
 @app.post("/product")
-def add_product(product: Product):
+def add_product(product: Product, db: Session = Depends(get_db)):
+    '''OLD:
     products.append(product)
-    return product
+    return product # returns what you added, pythonic!
+    '''
+    db.add(database_models.Product(**product.model_dump()))
+    return product # returns what you added, pythonic!
+
 
 @app.put("/product")
 def update_product(id: int, product: Product):
